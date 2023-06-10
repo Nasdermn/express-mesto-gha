7964ee3d58553d5b341eb853e5d25f9818a2cd52
Dan <thunderusnasdermn@yandex.ru>
2023-06-10 15:32:38 +0300

    14 sprint v.1.2: fix: errors status code

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 module.exports = (err, req, res, next) => {
-  const { status = 500, message } = err;
-  res.status(status).send({
-    message: status === 500 ? 'Ошибка сервера' : message,
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'Ошибка сервера' : message,
   });
   next();
 };
